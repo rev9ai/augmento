4010b1dc01509c092f8900b10098bc2ae0035586
asdkazmi <asdkazmi@gmail.com>
2024-01-18 15:43:12 +0500

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
 
 setuptools.setup(
     name = "augmento",
-    version = "0.0.1.dev1",
+    version = "0.0.1",
     author = "Syed Muhammad Asad",
     author_email = "asdkazmi@gmail.com",
     description = "Augmento is a Python library that provides a collection of image augmentation techniques for machine learning applications.",
@@ -13,16 +13,12 @@ setuptools.setup(
     long_description_content_type = "text/markdown",
     url = "https://github.com/rev9ai/augmento",
     classifiers = [
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
     package_dir = {"": "src"},
